0387af930d5f047d7dc0bc31ef825a36bf90ca4f
zdodev <iver0945@gmail.com>
2021-03-18 00:04:45 +0900

    feat: 모델 컨트롤러 설정.

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -1,10 +1,23 @@
+import Fluent
+import FluentPostgresDriver
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.databases.use(.postgres(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "zdo",
+        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        database: Environment.get("DATABASE_NAME") ?? "mydb"
+    ), as: .psql)
+    
+//    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+//        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+//        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+//    }
 
-    // register routes
+    app.migrations.add(CreateTodo())
+
     try routes(app)
 }
