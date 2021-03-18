1ed243738e6c664ff613a5cb2e00eecf71a52472
zdodev <iver0945@gmail.com>
2021-03-18 11:16:21 +0900

    chore: configure 데이터베이스 접근 코드 수정.

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -3,19 +3,10 @@ import FluentPostgresDriver
 import Vapor
 
 public func configure(_ app: Application) throws {
-    
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "zdo",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "mydb"
-    ), as: .psql)
-    
-//    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-//        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
-//        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-//    }
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    }
 
     app.migrations.add(CreateTodo())
 
