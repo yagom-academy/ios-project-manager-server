770fd635bb4e25bb9263f8e9ccf2b78fe9eddd70
yeon <jryoun0404@gmail.com>
2021-03-25 21:48:39 +0900

    [#9] Refactor: POST 요청 들어왔을 때 db에 저장하고, response body로 담아서 보내주도록 구현

--- ProjectManagerServer/Sources/App/Controllers/TodoController.swift
+++ ProjectManagerServer/Sources/App/Controllers/TodoController.swift
@@ -42,9 +42,22 @@ struct TodoController: RouteCollection {
             .unwrap(or: Abort(.notFound))
     }
     
-    func create(req: Request) throws -> EventLoopFuture<Todo> {
-        let todo = try req.content.decode(Todo.self)
-        return todo.save(on: req.db).map { todo }
+    func create(req: Request) throws -> EventLoopFuture<Response> {
+        try checkContentType(req)
+        let todoCreate = try req.content.decode(TodoCreate.self)
+        let todo = Todo(title: todoCreate.title,
+                           description: todoCreate.description,
+                           deadline: todoCreate.deadline,
+                           status: todoCreate.status)
+        
+        return todo.save(on: req.db).flatMapThrowing {
+            let header = ("Content-Type", "application/json; charset=utf-8")
+            let bodyJsonData = try encodeHTTPBody(todo)
+            
+            return Response(status: .created,
+                            headers: HTTPHeaders(dictionaryLiteral: header),
+                            body: .init(data: bodyJsonData))
+        }
     }
     
     func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
@@ -52,6 +65,14 @@ struct TodoController: RouteCollection {
             .unwrap(or: Abort(.notFound))
             .flatMap { $0.delete(on: req.db) }
             .transform(to: .ok)
+    }
+    
+    private func encodeHTTPBody(_ body: Todo) throws -> Data {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        let bodyJsonData = try encoder.encode(body)
+        
+        return bodyJsonData
     }
     
     private func checkContentType(_ req: Request) throws {
