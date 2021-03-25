eec74ece2a7a10bc82d4c5699dc6284b8b003742
yeon <jryoun0404@gmail.com>
2021-03-25 21:03:02 +0900

    [#5] Feat: Client로부터 받는 PATCH Request에 대한 TodoCreate 모델 생성

---
+++ ProjectManagerServer/Sources/App/Models/Request/TodoUpdate.swift
@@ -0,0 +1,14 @@
+//
+//  TodoUpdate.swift
+//  
+//
+//  Created by Yeon on 2021/03/25.
+//
+import Vapor
+
+struct TodoUpdate: Content {
+    let title: String?
+    let description: String?
+    let deadline: Date?
+    let status: Int?
+}
