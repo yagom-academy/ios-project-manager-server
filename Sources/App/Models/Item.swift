0f198818853ad5a196718084c8df8db70092c34a
lina0322 <llim@student.42seoul.kr>
2021-04-01 23:40:18 +0900

    Feat #4 모델 정의

--- Sources/App/Models/Item.swift
+++ Sources/App/Models/Item.swift
@@ -9,11 +9,27 @@ final class Item: Model, Content {
 
     @Field(key: "title")
     var title: String
+    
+    @Field(key: "body")
+    var body: String
+    
+    @Field(key: "state")
+    var state: String
 
+    @Field(key: "deadline")
+    var deadline: Number?
+    
+    @Field(key: "last_modified")
+    var last_modified: Number
+    
     init() { }
 
-    init(id: UUID? = nil, title: String) {
+    init(id: UUID? = nil, title: String, body: String, state: String, deadline: Number, last_modified: Number) {
         self.id = id
         self.title = title
+        self.body = body
+        self.state = state
+        self.deadline = deadline
+        self.last_modified = last_modified
     }
 }
