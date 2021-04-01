061f7adea747de4e6dc5c0b0aadf01e42a5b9cbe
lina0322 <llim@student.42seoul.kr>
2021-04-02 01:22:06 +0900

    Refactor #7 데이터 베이스와의 연결 방식 변경

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,36 +2,30 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+    
+    if let databaseURL = Environment.get("DATABASE_URL"),
+       var postgresConfig = PostgresConfiguration(url: databaseURL) {
         postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
-        app.databases.use(.postgres(
-            configuration: postgresConfig
+        app.databases.use(.postgres(configuration: postgresConfig
         ), as: .psql)
     } else {
-        // ...
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
     }
     
-    app.databases.use(.postgres(hostname: "localhost",
-                            username: "llim",
-                            password: "",
-                            database: "items"),
-                            as: .psql)
-//
-//    app.databases.use(.postgres(
-//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-//        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-//    ), as: .psql)
-//
+//    app.databases.use(.postgres(hostname: "localhost",
+//                                username: "llim",
+//                                password: "",
+//                                database: "items"),
+//                      as: .psql)    
+    
     app.migrations.add(CreateItem())
-
-    // register routes
+    
     try routes(app)
 }
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,1 @@
-//
-//  File.swift
-//  
-//
-//  Created by 리나 on 2021/04/01.
-//
 
-import Foundation
