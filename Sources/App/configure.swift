d8276557c1df4999acb99ebff1a053b7a705ed4d
lina0322 <llim@student.42seoul.kr>
2021-04-02 01:00:54 +0900

    Feat #7 데이터베이스 연결

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,14 +16,20 @@ public func configure(_ app: Application) throws {
         // ...
     }
     
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
-
+    app.databases.use(.postgres(hostname: "localhost",
+                            username: "llim",
+                            password: "",
+                            database: "items"),
+                            as: .psql)
+//
+//    app.databases.use(.postgres(
+//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+//        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+//    ), as: .psql)
+//
     app.migrations.add(CreateItem())
 
     // register routes
