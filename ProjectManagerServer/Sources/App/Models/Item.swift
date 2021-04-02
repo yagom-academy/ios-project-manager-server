95e2625ca0607d7eaafba3372c5a6dfd672bf7e2
sole <subuchappyv2@gmail.com>
2021-04-02 19:17:29 +0900

    feat: Model-Item Type 구현 (#3)

---
+++ ProjectManagerServer/Sources/App/Models/Item.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by sole on 2021/04/02.
+//
+
+import Fluent
+import Vapor
+
+final class Item: Model {
+    static let schema = "items"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Field(key: "status")
+    var status: Int
+    
+    @OptionalField(key: "description")
+    var description: String?
+    
+    @OptionalField(key: "deadline")
+    var deadline: Int?
+    
+    init() { }
+    
+    init(id: UUID? = nil,
+         title: String,
+         status: Status,
+         description: String? = nil,
+         deadline: Date? = nil) {
+        self.id = id
+        self.title = title
+        self.status = status
+        self.description = description
+        self.deadline = deadline
+    }
+}
