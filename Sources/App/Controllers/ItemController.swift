27a262f9c8c2307d9a7a4a614dffb5bb53078719
lina0322 <llim@student.42seoul.kr>
2021-04-06 00:17:01 +0900

    Refactor #12 CURD 및 boot 변경

--- Sources/App/Controllers/ItemController.swift
+++ Sources/App/Controllers/ItemController.swift
@@ -5,23 +5,41 @@ struct ItemController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let items = routes.grouped("items")
         items.get(use: readAll)
-        items.post(use: create)
-        items.group(":itemID") { item in
-            items.delete(use: delete)
+        
+        let item = routes.grouped("item")
+        item.post(use: create)
+        item.patch(":id", use: update)
+        item.delete(":id", use: delete)
+    }
+    
+    private func readAll(req: Request) throws -> EventLoopFuture<[ItemList]> {
+        try checkContentType(req.headers.contentType)
+        return Item.query(on: req.db).all().map { things -> [ItemList] in
+            let group = Dictionary(grouping: things, by: { $0.state })
+            var lists: [ItemList] = []
+            for (key, value) in group {
+                lists.append(ItemList(state: key, list: value))
+            }
+            return lists
         }
     }
-
-    func readAll(req: Request) throws -> EventLoopFuture<[Item]> {
-        return Item.query(on: req.db).all()
-    }
-
-    func create(req: Request) throws -> EventLoopFuture<Item> {
+    
+    private func create(req: Request) throws -> EventLoopFuture<Item> {
+        try checkContentType(req.headers.contentType)
         try Item.validate(content: req)
         let item = try req.content.decode(Item.self)
         return item.save(on: req.db).map { item }
     }
-
-    func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+    
+    private func update(req: Request) throws -> EventLoopFuture<Item> {
+        try checkContentType(req.headers.contentType)
+        try Item.validate(content: req)
+        let item = try req.content.decode(Item.self)
+        return item.update(on: req.db).map { item }
+    }
+    
+    private func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        try checkContentType(req.headers.contentType)
         return Item.find(req.parameters.get("itemID"), on: req.db)
             .unwrap(or: Abort(.notFound))
             .flatMap { $0.delete(on: req.db) }
