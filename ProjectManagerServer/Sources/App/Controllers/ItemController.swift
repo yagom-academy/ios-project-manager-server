0c6599ee7c397e4282a57f69461bab4813bda7a7
sole <subuchappyv2@gmail.com>
2021-04-06 02:12:37 +0900

    feat: ItmeController-get & post 수정

--- ProjectManagerServer/Sources/App/Controllers/ItemController.swift
+++ ProjectManagerServer/Sources/App/Controllers/ItemController.swift
@@ -10,9 +10,9 @@ import Vapor
 
 struct ItemController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        let items = routes.grouped("items")
-        items.get(use: showAll)
-        items.post(use: create)
+        let router = routes.grouped(Item.schema.pathComponents)
+        router.get(use: showAll)
+        router.post(use: create)
     }
     
     func showAll(req: Request) throws -> EventLoopFuture<[Item]> {
@@ -21,6 +21,6 @@ struct ItemController: RouteCollection {
     
     func create(req: Request) throws -> EventLoopFuture<Item> {
         let item = try req.content.decode(Item.self)
-        return item.create(on: req.db).map { item }
+        return item.save(on: req.db).map { item }
     }
 }
