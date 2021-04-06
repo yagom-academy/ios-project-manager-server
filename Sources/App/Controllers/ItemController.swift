3599de8f68404d891f9fb6f80692a4ee7e83f6b8
lina0322 <llim@student.42seoul.kr>
2021-04-06 13:37:43 +0900

    Fix delete와 init도 Int로 변경

--- Sources/App/Controllers/ItemController.swift
+++ Sources/App/Controllers/ItemController.swift
@@ -40,11 +40,7 @@ struct ItemController: RouteCollection {
 //
     private func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
         try checkContentType(req.headers.contentType)
-        
-        guard let id = UUID(uuidString: req.parameters.get("id")!) else {
-                    throw Abort(.badRequest)
-                }
-        
+        let id = try checkID(req)
         return Item.find(id, on: req.db)
             .unwrap(or: Abort(.notFound))
             .flatMap { $0.delete(on: req.db) }
@@ -57,10 +53,10 @@ struct ItemController: RouteCollection {
         }
     }
     
-//    private func checkID(_ req: Request) throws -> Int {
-//        guard let parameterID = req.parameters.get("id"), let id = Int(parameterID) else {
-//            throw Abort(.notFound)
-//        }
-//        return id
-//    }
+    private func checkID(_ req: Request) throws -> Int {
+        guard let parameterID = req.parameters.get("id"), let id = Int(parameterID) else {
+            throw Abort(.notFound)
+        }
+        return id
+    }
 }
