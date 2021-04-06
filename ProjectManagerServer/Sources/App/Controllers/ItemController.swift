ec1a7cebf53f183df97751b1de28ab43593fc692
sole <subuchappyv2@gmail.com>
2021-04-06 10:22:46 +0900

    style: Migration 네이밍 수정

--- ProjectManagerServer/Sources/App/Controllers/ItemController.swift
+++ ProjectManagerServer/Sources/App/Controllers/ItemController.swift
@@ -12,15 +12,9 @@ struct ItemController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let router = routes.grouped(Item.schema.pathComponents)
         router.get(use: showAll)
-        router.post(use: create)
     }
     
     func showAll(req: Request) throws -> EventLoopFuture<[Item]> {
         return Item.query(on: req.db).all()
-    }
-    
-    func create(req: Request) throws -> EventLoopFuture<Item> {
-        let item = try req.content.decode(Item.self)
-        return item.save(on: req.db).map { item }
     }
 }
