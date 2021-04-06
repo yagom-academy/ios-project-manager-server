7086629851842922aa6d8c880237a9a296346211
GREEN <humains29@gmail.com>
2021-04-06 10:41:57 +0900

    [#4] feat: 마이그레이션 정의

---
+++ ProjectManagerServer/Sources/App/Migration/CreateThing.swift
@@ -0,0 +1,26 @@
+import Fluent
+
+struct CreateThing: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        _ = database.enum("state")
+            .case("todo")
+            .case("doing")
+            .case("done")
+            .create()
+        
+        return database.enum("state").read().flatMap { state in
+            database.schema("things")
+                .id()
+                .field("title", .string)
+                .field("description", .string)
+                .field("state", state, .required)
+                .field("due_date", .double, .required)
+                .field("updated_at", .datetime, .required)
+                .create()
+        }
+    }
+    
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        return database.schema("things").delete()
+    }
+}
