c538ac3bd3be54480e14094ff181d45abf39b6c0
GREEN <humains29@gmail.com>
2021-04-08 09:57:14 +0900

    [#7] feat: DB_URL 접근 방식에 대한 else문 추가

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,6 +9,13 @@ public func configure(_ app: Application) throws {
             configuration: postgresConfig
         ), as: .psql)
     } else {
+        app.databases.use(.postgres(
+                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+                username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+                database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+            ), as: .psql)
     }
     
     app.migrations.add(CreateThing())
