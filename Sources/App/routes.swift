b9319d7c24051704cebb6fe340e306ad0a164627
GREEN <humains29@gmail.com>
2021-04-08 09:46:19 +0900

    [#7] refactor: GET API 통신 시 hello 예시 코드 제거

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,10 +4,6 @@ func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
     
     try app.register(collection: ThingController())
 }
