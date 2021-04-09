afa404955c2df2e8c33c493ec9d47f28e37df6b6
innieminnie <rkddlsl1001@gmail.com>
2021-04-09 18:54:57 +0900

    Fix readAll에서 checkContentType 삭제

--- Sources/App/Controllers/ItemController.swift
+++ Sources/App/Controllers/ItemController.swift
@@ -16,7 +16,6 @@ struct ItemController: RouteCollection {
     }
     
     private func readAll(req: Request) throws -> EventLoopFuture<Response> {
-        try checkContentType(req.headers.contentType)
         return Item.query(on: req.db).all().flatMapThrowing { things in
             let group = Dictionary(grouping: things, by: { $0.state })
             var lists: [ItemList] = []
