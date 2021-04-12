5f3e94c434de335321f7b3c61fd653117ff782f2
SM <hcooch2ch3@gmail.com>
2021-04-12 22:20:35 +0900

    Remove: routes.swift에 불필요한 경로 삭제

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,16 +1,5 @@
 import Vapor
 
-func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
-    
-    /// URL: /things
-    /// GET: 전체 thing 반환
-    /// POST: 새로운 thing 생성
-    /// DELETE: 해당 thing 삭제
-    /// PATCH: 해당 thing 수정
-    
 func routes(_ app: Application) throws {    
     try app.register(collection: ThingController(url: "things"))
 }
