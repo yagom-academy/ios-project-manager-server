df1eb7b38e204b6da1509d061f80ad49ed34c91f
SM <hcooch2ch3@gmail.com>
2021-04-14 15:59:50 +0900

    Feat: ThingError 구현

--- Sources/App/Controllers/ThingError.swift
+++ Sources/App/Controllers/ThingError.swift
@@ -1,8 +1,38 @@
 //
-//  File.swift
+//  ThingError.swift
 //  
 //
 //  Created by 임성민 on 2021/04/13.
 //
 
-import Foundation
+import Vapor
+
+enum ThingError {
+    case invalidState
+    case notFoundID
+    case tooLongDescription
+}
+
+extension ThingError: AbortError {
+    var reason: String {
+        switch self {
+        case .invalidState:
+            return "State is not valid."
+        case .notFoundID:
+            return "ID is not exist."
+        case .tooLongDescription:
+            return "Description cannot exceed 1000 characters."
+        }
+    }
+    
+    var status: HTTPResponseStatus {
+        switch self {
+        case .invalidState:
+            return .badRequest
+        case .notFoundID:
+            return .notFound
+        case .tooLongDescription:
+            return .badRequest
+        }
+    }
+}
