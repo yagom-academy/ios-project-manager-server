d7c3f2e5c1f45ec019ee5d3825711a226918f241
lina0322 <llim@student.42seoul.kr>
2021-04-15 20:22:42 +0900

    Feat #15 post failure test case 구현

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -39,4 +39,48 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.status, .created)
         }
     }
+
+    func testPostFailureCase_longTitle() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        let longTitle = "title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. "
+        let item = Item(title: longTitle, body: "body", state: .todo, deadline: nil)
+        let body = try jsonEncoder.encodeAsByteBuffer(item, allocator: ByteBufferAllocator())
+        
+        try app.test(.POST, "item", headers: header, body: body) { res in
+            XCTAssertEqual(res.status, .badRequest)
+        }
+    }
+    
+    func testPostFailureCase_longBody() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        let longBody = "Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다."
+        let item = Item(title: "title", body: longBody, state: .todo, deadline: nil)
+        let body = try jsonEncoder.encodeAsByteBuffer(item, allocator: ByteBufferAllocator())
+        
+        try app.test(.POST, "item", headers: header, body: body) { res in
+            XCTAssertEqual(res.status, .badRequest)
+        }
+    }
+
+    func testPatchSuccessCase() throws {
+        
+    }
+
+    func testPatchFailureCase() throws {
+        
+    }
+
+    func testDeleteSuccessCase() throws {
+        
+    }
+
+    func testDeleteFailureCase() throws {
+        
+    }
 }
