f73e4cec6ca335cb5b517a40ecdfe4d6e07a3bbd
lina0322 <llim@student.42seoul.kr>
2021-04-15 21:08:15 +0900

    Feat #15 delete failure test case 구현

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -10,28 +10,28 @@ final class AppTests: XCTestCase {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
+        
         try app.test(.GET, "items", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
         })
     }
-
+    
     func testGetFailureCase() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
+        
         let invalidURI = "item"
         try app.test(.GET, invalidURI, afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
-
+    
     func testPostSuccessCase() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
+       
         let item = Item(title: "title", body: "body", state: .todo, deadline: nil)
         let body = try jsonEncoder.encodeAsByteBuffer(item, allocator: ByteBufferAllocator())
         
@@ -39,12 +39,12 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.status, .created)
         }
     }
-
+    
     func testPostFailureCase_longTitle() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
+        
         let longTitle = "title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. title이 500자 이상이면 post할 수 없습니다. "
         let item = Item(title: longTitle, body: "body", state: .todo, deadline: nil)
         let body = try jsonEncoder.encodeAsByteBuffer(item, allocator: ByteBufferAllocator())
@@ -58,7 +58,7 @@ final class AppTests: XCTestCase {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
+        
         let longBody = "Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다. Body가 1000자 이상이면 post할 수 없습니다."
         let item = Item(title: "title", body: longBody, state: .todo, deadline: nil)
         let body = try jsonEncoder.encodeAsByteBuffer(item, allocator: ByteBufferAllocator())
@@ -67,17 +67,27 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.status, .badRequest)
         }
     }
-
+    
     func testPatchFailureCase() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
+        
         let item = Item(title: "New title", body: "New body", state: .doing, deadline: nil)
         let body = try jsonEncoder.encodeAsByteBuffer(item, allocator: ByteBufferAllocator())
-
+        
         try app.test(.PATCH, "item", headers: header, body: body) { res in
             XCTAssertEqual(res.status, .notFound)
         }
+    }
+
+    func testDeleteFailureCase() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+        
+        try app.test(.DELETE, "items", afterResponse: { res in
+            XCTAssertEqual(res.status, .notFound)
+        })
     }
 }
