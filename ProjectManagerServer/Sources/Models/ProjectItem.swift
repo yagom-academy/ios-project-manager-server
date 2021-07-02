999e4bfbbe04b79a9bb88d6100e433f27af6dc56
kane-young <lyw2100@naver.com>
2021-07-02 10:45:02 +0900

    Feat : ProjectItem model 타입 생성

---
+++ ProjectManagerServer/Sources/Models/ProjectItem.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이영우 on 2021/07/02.
+//
+
+import Fluent
+import Vapor
+
+final class ProjectItem: Model, Content {
+    static let schema = "projectItems"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Field(key: "content")
+    var content: String
+    
+    @Field(key: "deadlineDate")
+    var deadlineDate: Date
+    
+    @Field(key: "progress")
+    var progress: String
+    
+    @Field(key: "isDeleted")
+    var isDeleted: Bool
+    
+    init() { }
+    
+    init(id: UUID? = nil, title: String, content: String, deadlineDate: Date, progress: String, isDeleted: Bool) {
+        self.id = id
+        self.title = title
+        self.content = content
+        self.deadlineDate = deadlineDate
+        self.progress = progress
+        self.isDeleted = isDeleted
+    }
+}
