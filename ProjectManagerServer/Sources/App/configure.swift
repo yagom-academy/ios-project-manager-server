76cf54107828775f549166778f96ada78bfa17f3
ryan-son <ryan.son1002@gmail.com>
2021-07-02 17:54:33 +0900

    Feat: Heroku Postgres 데이터베이스 연결 구문 추가

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -2,14 +2,18 @@ import Vapor
 import Fluent
 import FluentPostgresDriver
 
-// configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.migrations.add(CreateProjectItem())
     
-    app.databases.use(.postgres(hostname: "localhost", username: "ryan-son", password: "", database: "project-management-appserver"), as: .psql)
-
-    // register routes
+    let databaseURL = "postgres://krlhuxlszenkwz:984a411cee1a56ca3f467c9a247749569b7be24476e9cd68ba9f1d9a35e59003@ec2-34-202-54-225.compute-1.amazonaws.com:5432/d4s708j7913lfc"
+    
+    if let databaseURL = Environment.get(databaseURL) {
+        app.databases.use(try .postgres(
+            url: databaseURL
+        ), as: .psql)
+    } else {
+        throw Abort(.internalServerError)
+    }
+    
     try routes(app)
 }
