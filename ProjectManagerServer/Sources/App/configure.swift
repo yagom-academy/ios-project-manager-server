3bee9868ee1c9f91adb96f1b29fb6711ec0f9634
ryan-son <ryan.son1002@gmail.com>
2021-07-05 18:27:40 +0900

    Chore: Database 구성을 local에서 heroku로 변경

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -10,21 +10,16 @@ import Fluent
 import FluentPostgresDriver
 
 public func configure(_ app: Application) throws {
-    app.migrations.add(CreateProjectItem() , to: .psql)
+    app.migrations.add(CreateProjectItem(), to: .psql)
     
-    app.databases.use(.postgres(hostname: "localhost",
-                                username: "ryan-son",
-                                password: "",
-                                database: "project-management-appserver"),
-                      as: .psql)
-//    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-//        var clientTLSConfiguration = TLSConfiguration.makeClientConfiguration()
-//        clientTLSConfiguration.certificateVerification = .none
-//        postgresConfig.tlsConfiguration = clientTLSConfiguration
-//        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-//    } else {
-//        throw Abort(.internalServerError)
-//    }
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        var clientTLSConfiguration = TLSConfiguration.makeClientConfiguration()
+        clientTLSConfiguration.certificateVerification = .none
+        postgresConfig.tlsConfiguration = clientTLSConfiguration
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        throw Abort(.internalServerError)
+    }
     
     try routes(app)
 }
