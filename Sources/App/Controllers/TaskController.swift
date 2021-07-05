dd0bb5a93e566c64b9c6264f3a893a0e6c94b6da
stevenkim18 <11882@naver.com>
2021-07-05 14:26:03 +0900

    feat: task 생성 api 구현

--- Sources/App/Controllers/TaskController.swift
+++ Sources/App/Controllers/TaskController.swift
@@ -12,10 +12,15 @@ struct TaskController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let tasks = routes.grouped("tasks")
         tasks.get(use: showAll)
+        tasks.post(use: create)
     }
     
     func showAll(req: Request) throws -> EventLoopFuture<[Task]> {
         return Task.query(on: req.db).all()
     }
     
+    func create(req: Request) throws -> EventLoopFuture<Task> {
+        let task = try req.content.decode(Task.self)
+        return task.create(on: req.db).map { task }
+    }
 }
