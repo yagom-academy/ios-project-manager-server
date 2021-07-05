a79f9c5e46cd1bd2ce79c3a3ccafc842fcd18354
duckbok <duckbok22@gmail.com>
2021-07-05 17:15:06 +0900

    feat: patch시에 수정된 항목이 없을때 update 방지

--- Sources/App/Controllers/TaskController.swift
+++ Sources/App/Controllers/TaskController.swift
@@ -18,41 +18,58 @@ struct TaskController: RouteCollection {
             tasks.delete(use: delete)
         }
     }
-    
+
     func showAll(req: Request) throws -> EventLoopFuture<[Task]> {
         return Task.query(on: req.db).all()
     }
-    
+
     func create(req: Request) throws -> EventLoopFuture<Task> {
         let task = try req.content.decode(Task.self)
         return task.create(on: req.db).map { task }
     }
-    
+
     func update(req: Request) throws -> EventLoopFuture<Task> {
+        guard let id = req.parameters.get("id", as: Int.self) else { throw Abort(.notFound) }
         let patchTask = try req.content.decode(PatchTask.self)
-        return Task.find(req.parameters.get("id"), on: req.db)
+
+        return Task.find(id, on: req.db)
             .unwrap(or: Abort(.notFound))
             .flatMap { task in
-                if let title = patchTask.title {
+                var isChanged: Bool = false
+
+                if let title = patchTask.title,
+                   task.title != title {
                     task.title = title
+                    isChanged = true
                 }
-                
-                if let deadline = patchTask.deadline {
+
+                if let deadline = patchTask.deadline,
+                   task.deadline != deadline {
                     task.deadline = deadline
+                    isChanged = true
                 }
-                
-                if let state = patchTask.state {
+
+                if let state = patchTask.state,
+                   task.state != state {
                     task.state = state
+                    isChanged = true
                 }
-                
-                if let contents = patchTask.contents {
+
+                if let contents = patchTask.contents,
+                   task.contents != contents {
                     task.contents = contents
+                    isChanged = true
                 }
-                
+
+                guard isChanged else {
+                    return Task.find(id, on: req.db)
+                        .unwrap(or: Abort(.notFound))
+                }
+
                 return task.update(on: req.db).transform(to: task)
             }
     }
-    
+
     func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
         return Task.find(req.parameters.get("id"), on: req.db)
                     .unwrap(or: Abort(.notFound))
