7b03431f6d65df844816c5fa960d9e02d0e89469
Neph <tony3322@naver.com>
2021-07-07 15:01:05 +0900

    [Feat] GET, POST, PATCH, DELETE 1차 구현

--- Sources/App/Controllers/MemoController.swift
+++ Sources/App/Controllers/MemoController.swift
@@ -11,14 +11,57 @@ import Vapor
 struct MemoController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let memo = routes.grouped("memo")
-        memo.get { req in
-            return "memo페이지"
-        }
+        memo.get("todo", ":pageId", use: getToDo)
+        memo.get("doing", ":pageId", use: getDoing)
+        memo.get("done", ":pageId", use: getDone)
+        
         memo.post(use: create)
+        memo.patch(":memoId", use: update)
+        memo.delete(":memoId", use: delete)
+    }
+    
+    
+    func getToDo(request: Request) throws -> EventLoopFuture<[Memo]> {
+        return Memo.query(on: request.db)
+            .filter(\.$memo_type == .toDo)
+            .all()
+    }
+    
+    func getDoing(request: Request) throws -> EventLoopFuture<[Memo]> {
+        return Memo.query(on: request.db)
+            .filter(\.$memo_type == .doing)
+            .all()
+    }
+    
+    func getDone(request: Request) throws -> EventLoopFuture<[Memo]> {
+        return Memo.query(on: request.db)
+            .filter(\.$memo_type == .done)
+            .all()
     }
     
     func create(request: Request) throws -> EventLoopFuture<Memo> {
         let memo = try request.content.decode(Memo.self)
         return memo.create(on: request.db).map { memo }
+    }
+    
+    func update(request: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let memo = try request.content.decode(Memo.self)
+        return Memo.find(request.parameters.get("memoId"), on: request.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap {
+                $0.title = memo.title
+                $0.content = memo.content
+                $0.due_date = memo.due_date
+                return $0.update(on: request.db).transform(to: .ok)
+            }
+    }
+    
+    func delete(request: Request) throws -> EventLoopFuture<HTTPStatus> {
+
+        return Memo.find(request.parameters.get("memoId"), on: request.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap {
+                return $0.delete(on: request.db) // 여기에 transform 적었더니 오류
+            }.transform(to: .ok)
     }
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,14 +3,21 @@ import Fluent
 import FluentPostgresDriver
 
 public func configure(_ app: Application) throws {
-    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
-        app.databases.use(.postgres(
-            configuration: postgresConfig
-        ), as: .psql)
-    } else {
-        // ...
-    }
+//    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+//        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+//        app.databases.use(.postgres(
+//            configuration: postgresConfig
+//        ), as: .psql)
+//    } else {
+//        // ...
+//    }
+    
+    app.databases.use(
+        .postgres(hostname: "localhost",
+                  username: "postgres",
+                  password: "",
+                  database: "memos"),
+        as: .psql)
 
     app.migrations.add(MemoMigration())
     try routes(app)
