4faf34482bf12ca67f4173c301fd2f546bfe9e23
Neph <tony3322@naver.com>
2021-07-07 17:01:54 +0900

    [Fix] heroku 오류로 인해 예전 메서드로 다시 사용

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,7 +4,7 @@ import FluentPostgresDriver
 
 public func configure(_ app: Application) throws {
     if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-        postgresConfig.tlsConfiguration = .makeClientConfiguration()
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
         app.databases.use(.postgres(
             configuration: postgresConfig
         ), as: .psql)
