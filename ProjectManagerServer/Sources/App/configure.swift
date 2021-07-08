d61e347dac3744cc48933988746b6eddd5a5d379
ryan-son <ryan.son1002@gmail.com>
2021-07-08 15:25:03 +0900

    Chore : DB 구성을 로컬에서 원격으로 변경

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -12,16 +12,14 @@ import FluentPostgresDriver
 public func configure(_ app: Application) throws {
     app.migrations.add(CreateProjectItem(), to: .psql)
     
-//    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-//        var clientTLSConfiguration = TLSConfiguration.makeClientConfiguration()
-//        clientTLSConfiguration.certificateVerification = .none
-//        postgresConfig.tlsConfiguration = clientTLSConfiguration
-//        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-//    } else {
-//        throw Abort(.internalServerError)
-//    }
-    
-    app.databases.use(.postgres(hostname: "localhost", username: "ryan-son", password: "", database: "project-management-appserver"), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        var clientTLSConfiguration = TLSConfiguration.makeClientConfiguration()
+        clientTLSConfiguration.certificateVerification = .none
+        postgresConfig.tlsConfiguration = clientTLSConfiguration
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        throw Abort(.internalServerError)
+    }
     
     try routes(app)
 }
