42b0773b5c90a597bd43dec349e982fe9741013d
Neph <tony3322@naver.com>
2021-07-08 17:18:25 +0900

    [Chore] heroku 배포 위해 local 접속코드 삭제

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,24 +3,17 @@ import Fluent
 import FluentPostgresDriver
 
 public func configure(_ app: Application) throws {
-//    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-//        var clientTLSConfiguration = TLSConfiguration.makeClientConfiguration()
-//        clientTLSConfiguration.certificateVerification = .none
-//        postgresConfig.tlsConfiguration = clientTLSConfiguration
-//
-//        app.databases.use(.postgres(
-//            configuration: postgresConfig
-//        ), as: .psql)
-//    } else {
-//        throw Abort(.custom(code: 500, reasonPhrase: "configuration failed"))
-//    }
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        var clientTLSConfiguration = TLSConfiguration.makeClientConfiguration()
+        clientTLSConfiguration.certificateVerification = .none
+        postgresConfig.tlsConfiguration = clientTLSConfiguration
 
-        app.databases.use(
-            .postgres(hostname: "localhost",
-                      username: "postgres",
-                      password: "",
-                      database: "memos"),
-            as: .psql)
+        app.databases.use(.postgres(
+            configuration: postgresConfig
+        ), as: .psql)
+    } else {
+        throw Abort(.custom(code: 500, reasonPhrase: "configuration failed"))
+    }
     app.migrations.add(MemoMigration())
     try routes(app)
 }
