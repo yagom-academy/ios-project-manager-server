a88e14e9ea55f913e5bb614705a879c5c0e8ec57
ryan-son <ryan.son1002@gmail.com>
2021-07-09 10:59:42 +0900

    Test : Delete 테스트 케이스 추가

--- ProjectManagerServer/Tests/AppTests/AppTests.swift
+++ ProjectManagerServer/Tests/AppTests/AppTests.swift
@@ -10,27 +10,25 @@ import XCTVapor
 
 final class AppTests: XCTestCase {
     
-    var app: Application!
+    private var app: Application!
     
     override func setUpWithError() throws {
+        print("시작")
         app = try Application.testable()
     }
     
     override func tearDownWithError() throws {
         app.shutdown()
+        print("끝")
     }
     
     func testProjectItemCanBeRetrievedFromAPI() throws {
-        let app = try Application.testable()
-        defer { app.shutdown() }
-        
         try TestAssets.projectItem.save(on: app.db).wait()
         
         try app.test(.GET, TestAssets.todoProjectItemsURI, afterResponse: { response in
             XCTAssertEqual(response.status, .ok)
             
             let todoProjectItems = try response.content.decode([ProjectItem].self)
-            
             XCTAssertEqual(todoProjectItems.count, 1)
             XCTAssertEqual(todoProjectItems[0].title, TestAssets.expectedTitle)
             XCTAssertEqual(todoProjectItems[0].content, TestAssets.expectedContent)
@@ -44,8 +42,9 @@ final class AppTests: XCTestCase {
         try app.test(.POST, TestAssets.projectItemURI, beforeRequest: { req in
             try req.content.encode(TestAssets.projectItem)
         }, afterResponse: { response in
-            let receivedProjectItem = try response.content.decode(ProjectItem.self)
+            XCTAssertEqual(response.status, .ok)
             
+            let receivedProjectItem = try response.content.decode(ProjectItem.self)
             XCTAssertEqual(receivedProjectItem.title, TestAssets.expectedTitle)
             XCTAssertEqual(receivedProjectItem.content, TestAssets.expectedContent)
             XCTAssertEqual(receivedProjectItem.deadlineDate, TestAssets.expectedDeadlineDate)
@@ -53,6 +52,8 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(receivedProjectItem.index, TestAssets.expectedIndex)
             
             try app.test(.GET, TestAssets.todoProjectItemsURI, afterResponse: { secondResponse in
+                XCTAssertEqual(secondResponse.status, .ok)
+                
                 let todoProjectItems = try secondResponse.content.decode([ProjectItem].self)
                 XCTAssertEqual(todoProjectItems.count, 1)
                 XCTAssertEqual(todoProjectItems[0].title, TestAssets.expectedTitle)
@@ -66,7 +67,7 @@ final class AppTests: XCTestCase {
     
     func testProjectItemCanBeUpdatedWithAPI() throws {
         let projectItem = TestAssets.projectItem
-        let _ = projectItem.save(on: app.db)
+        let _ = try projectItem.save(on: app.db).wait()
         let patchProjectItem = PatchProjectItem(id: try projectItem.requireID(),
                                                 title: "업데이트 됐어요!",
                                                 content: "성공적이야!",
@@ -77,10 +78,9 @@ final class AppTests: XCTestCase {
         try app.test(.PATCH, TestAssets.projectItemURI, beforeRequest: { req in
             try req.content.encode(patchProjectItem)
         }, afterResponse: { response in
-            let receivedProjectItem = try response.content.decode(ProjectItem.self)
-            
             XCTAssertEqual(response.status, .ok)
             
+            let receivedProjectItem = try response.content.decode(ProjectItem.self)
             XCTAssertEqual(receivedProjectItem.title, TestAssets.expectedPatchTitle)
             XCTAssertEqual(receivedProjectItem.content, TestAssets.expectedPatchContent)
             XCTAssertEqual(receivedProjectItem.deadlineDate, TestAssets.expectedDeadlineDate)
@@ -88,6 +88,8 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(receivedProjectItem.index, TestAssets.expectedPatchIndex)
             
             try app.test(.GET, TestAssets.doingProjectItemsURI, afterResponse: { secondResponse in
+                XCTAssertEqual(secondResponse.status, .ok)
+                
                 let doingProjectItems = try secondResponse.content.decode([ProjectItem].self)
                 XCTAssertEqual(doingProjectItems.count, 1)
                 XCTAssertEqual(doingProjectItems[0].title, TestAssets.expectedPatchTitle)
@@ -95,6 +97,25 @@ final class AppTests: XCTestCase {
                 XCTAssertEqual(doingProjectItems[0].deadlineDate, TestAssets.expectedDeadlineDate)
                 XCTAssertEqual(doingProjectItems[0].progress, TestAssets.expectedPatchProgress)
                 XCTAssertEqual(doingProjectItems[0].index, TestAssets.expectedPatchIndex)
+            })
+        })
+    }
+    
+    func testProjectItemCanBeDeletedWithAPI() throws {
+        let projectItem = TestAssets.projectItem
+        let _ = try projectItem.save(on: app.db).wait()
+        let deleteProjectItem = DeleteProjectItem(id: try projectItem.requireID())
+        
+        try app.test(.DELETE, TestAssets.projectItemURI, beforeRequest: { req in
+            try req.content.encode(deleteProjectItem)
+        }, afterResponse: { response in
+            XCTAssertEqual(response.status, .ok)
+            
+            try app.test(.GET, TestAssets.todoProjectItemsURI, afterResponse: { secondResponse in
+                XCTAssertEqual(secondResponse.status, .ok)
+                
+                let todoProjectItems = try secondResponse.content.decode([ProjectItem].self)
+                XCTAssertEqual(todoProjectItems.count, 0)
             })
         })
     }
