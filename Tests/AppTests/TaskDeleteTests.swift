2054bb934c3c45c3a02bf85b190b5ccd060c3bf5
duckbok <duckbok22@gmail.com>
2021-07-14 10:33:09 +0900

    test: 숫자가 아닌 ID에 대한 삭제 요청

--- Tests/AppTests/TaskDeleteTests.swift
+++ Tests/AppTests/TaskDeleteTests.swift
@@ -16,6 +16,11 @@ final class TaskDeleteTests: XCTestCase {
         try configure(app)
         try app.autoRevert().wait()
         try app.autoMigrate().wait()
+
+        // given
+        let task = Task(title: "제목", deadline: Date(), state: .todo)
+        try task.save(on: app.db).wait()
+
         try super.setUpWithError()
     }
 
@@ -25,10 +30,6 @@ final class TaskDeleteTests: XCTestCase {
     }
 
     func test_Task_삭제가_성공했을때_204상태코드와_함께_task가_삭제된다() throws {
-        // given
-        let task = Task(title: "제목", deadline: Date(), state: .todo)
-        try task.save(on: app.db).wait()
-
         // when
         try app.test(.DELETE, "/tasks/1", afterResponse: { response in
             // then
@@ -39,12 +40,18 @@ final class TaskDeleteTests: XCTestCase {
     }
 
     func test_삭제요청한_ID의_Task가_없다면_404상태코드와_함께_에러를_응답한다() throws {
-        // given
-        let task = Task(title: "제목", deadline: Date(), state: .todo)
-        try task.save(on: app.db).wait()
-
         // when
         try app.test(.DELETE, "/tasks/2", afterResponse: { response in
+            // then
+            let isDeleted: Bool = try app.db.query(Task.self).all().wait().isEmpty
+            XCTAssertTrue(!isDeleted)
+            XCTAssertEqual(response.status, .notFound)
+        })
+    }
+
+    func test_삭제요청한_ID가_숫자가_아니라면_400상태코드와_함께_에러를_응답한다() throws {
+        // when
+        try app.test(.DELETE, "/tasks/abc", afterResponse: { response in
             // then
             let isDeleted: Bool = try app.db.query(Task.self).all().wait().isEmpty
             XCTAssertTrue(!isDeleted)
