dae0b17164c9d2d6f4173fb200e493f0b7d00fa5
dacodaco <imochi@naver.com>
2021-07-23 17:03:19 +0900

    [#2]style: configure 파일 가독성을 위한 수정

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,17 +7,14 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    if let databaseURL = Environment.get("postgres://ywommofyxcfnjo:501a6c902f36c547858fb305f1e47685909263fbc33334f4119795412ddecdef@ec2-52-71-231-37.compute-1.amazonaws.com:5432/dam3cth9soo9st"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+    let rawDatabaseURL = "postgres://ywommofyxcfnjo:501a6c902f36c547858fb305f1e47685909263fbc33334f4119795412ddecdef@ec2-52-71-231-37.compute-1.amazonaws.com:5432/dam3cth9soo9st"
+    
+    if let databaseURL = Environment.get(rawDatabaseURL),
+       var postgresConfig = PostgresConfiguration(url: databaseURL) {
         postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
-        app.databases.use(.postgres(
-            configuration: postgresConfig
-        ), as: .psql)
-    } else {
-        return
-    }
-    
-    
-    
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else { return }
+
     // register routes
     try routes(app)
 }
