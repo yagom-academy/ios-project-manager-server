a1358bea2786db55b8dfb42fdb690d4362a8d0c0
unme88 <sunny.on.a.roll@gmail.com>
2021-07-23 16:43:47 +0900

    [#2] PostgreSQL 설정

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,23 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    
+    if let databaseURL = Environment.get("postgres://ywommofyxcfnjo:501a6c902f36c547858fb305f1e47685909263fbc33334f4119795412ddecdef@ec2-52-71-231-37.compute-1.amazonaws.com:5432/dam3cth9soo9st"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(
+            configuration: postgresConfig
+        ), as: .psql)
+    } else {
+        return
+    }
+    
+    
+    
     // register routes
     try routes(app)
 }
