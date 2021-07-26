6d2cf07397684b1baa3cc043f77f1e99cbc96b52
dacodaco <imochi@naver.com>
2021-07-26 16:49:54 +0900

    test: It works get 확인

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,6 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
-    }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+        return "It works!!!!!!"
     }
 }
