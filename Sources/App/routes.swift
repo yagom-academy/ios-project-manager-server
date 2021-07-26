cfbd66bc748626a78ef231055ee81467e0903ba2
dacodaco <imochi@naver.com>
2021-07-26 17:04:58 +0900

    feat: create test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,4 +4,12 @@ func routes(_ app: Application) throws {
     app.get { req in
         return "It works!!!!!!"
     }
+    
+    let tasks = app.grouped("tasks")
+    tasks.post(use: create)
+}
+
+func create(req: Request) throws -> EventLoopFuture<Task> {
+    let task = try req.content.decode(Task.self)
+    return task.create(on: req.db).map { task }
 }
