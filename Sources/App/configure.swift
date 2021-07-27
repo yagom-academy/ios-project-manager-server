ada45ede991d3ded2d5b8874bd8b0d1cb999f295
dacodaco <imochi@naver.com>
2021-07-27 15:05:41 +0900

    localhost 에서 heroku 로 DB 변경

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,14 +7,14 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-//    if let databaseURL = Environment.get("DATABASE_URL"),
-//       var postgresConfig = PostgresConfiguration(url: databaseURL) {
-//        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
-//        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-//    } else { return }
+    if let databaseURL = Environment.get("DATABASE_URL"),
+       var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else { return }
     
-    app.databases.use(.postgres(hostname: "localhost", username: "postgres", password: "", database: "testdb"), as: .psql)
-    
+//    app.databases.use(.postgres(hostname: "localhost", username: "postgres", password: "", database: "testdb"), as: .psql)
+
     app.migrations.add(CreateTask())
 
     // register routes
