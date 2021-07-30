54c295b5b33187afff3ddb9bd19f96d3a9cb4e91
babywalnut <anfuza@naver.com>
2021-07-30 18:02:36 +0900

    [#2]feat: Database 마이그레이션 heroku 설정 configure.swift: 	- configure메서드에 database 설정을 로컬에서 heroku로 변경

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -4,8 +4,15 @@ import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
-        app.databases.use(.postgres(hostname: "localhost", username: "babywalnut", password: "", database: "project_manager_database"), as: .psql)
-        app.migrations.add(CreateTask())
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        app.databases.use(try .postgres(
+            url: databaseURL
+        ), as: .psql)
+    } else {
+        // ...
+    }
+
+    app.migrations.add(CreateTask())
     
     // register routes
     try routes(app)
