7fd393f59fe41fcba584c36cc1ea30a308783345
babywalnut <anfuza@naver.com>
2021-07-30 17:18:24 +0900

    [#2]feat: PostgreSQL DB 로컬 마이그레이션 설정, 테스트 configure.swift: 	- configure 메서드 내에 database 설정 값을 로컬DB로 설정
    routes.swift:
    	- 각 요청에 대한 반환값 수정
    
    PostgreSQL 로컬 DB에 데이터 주입:
    	- 2개의 레코드 추가

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -4,18 +4,8 @@ import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
-        app.databases.use(.postgres(
-            configuration: postgresConfig
-        ), as: .psql)
+        app.databases.use(.postgres(hostname: "localhost", username: "babywalnut", password: "", database: "project_manager_database"), as: .psql)
         app.migrations.add(CreateTask())
-    } else {
-        // ...
-    }
     
     // register routes
     try routes(app)
--- ProjectManagerServer/Sources/App/routes.swift
+++ ProjectManagerServer/Sources/App/routes.swift
@@ -2,10 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "안녕하세요"
+        return "Index page"
     }
 
     app.get("hello") { req -> String in
-        return "Hello, world!"
+        return "Hello, world! babian"
     }
 }
